d3d941752b0a59430ce46d70cef99e8fbc3b22b8
giulpio <giulpiomilani@gmail.com>
2019-02-10 16:50:23 +0100

    initial push

---
+++ ees_tree_no_open/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'EESTISOFT tree no open',
+    'version': '12.0.4.8',
+    'author': 'EESTISOFT',
+    'category': 'Productivity',
+    'website': 'https://eestisoft.com',
+    'sequence': 2,
+    'summary': 'Allows to set "no open" attribute on tree view',
+    'description': """
+Allows to set "no open" attribute on tree view to prevent interraction upon row click
+	
+Made with love.
+    """,
+    'depends': ['base'],
+    'data': ['static/views/view.xml'],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
