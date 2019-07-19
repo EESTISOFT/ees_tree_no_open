47b66c05f18cf859a4a96d22c932457157f0f01a
Hideki Yamamoto <hideki.yamamoto@gmail.com>
2019-07-19 22:05:15 +0000

    Update __manifest__.py

--- ees_tree_no_open/__manifest__.py
+++ ees_tree_no_open/__manifest__.py
@@ -5,13 +5,13 @@
     'category': 'Productivity',
     'website': 'https://eestisoft.com',
     'sequence': 2,
-    'summary': 'Allows to set "no open" attribute on tree view',
+    'summary': 'Allows to set "no open" attribute on tree view',    
+    'images':['static/description/thumb.png'],
     'description': """
 Allows to set "no open" attribute on tree view to prevent interraction upon row click
 	
 Made with love.
     """,
-	'images':['thumb.png'],
     'depends': ['base'],
     'data': ['static/views/view.xml'],
     'installable': True,
